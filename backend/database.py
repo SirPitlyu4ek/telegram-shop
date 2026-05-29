8495455a19cd8268efaab3d121136562579238ed
SirPitlyu4ek <vladislavpitlyuk@gmail.com>
2026-05-29 17:02:34 +0300

    Move database config to env

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,11 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:pitlyuk99@localhost:5432/telegram_shop"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
