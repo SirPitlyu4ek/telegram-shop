5212336108f2692b26cbc142005611db0592f5b3
SirPitlyu4ek <vladislavpitlyuk@gmail.com>
2026-06-03 00:10:22 +0300

    Added React product catalog

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
 
@@ -26,6 +27,17 @@ from integrations.wayforpay import (
 )
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class OrderCreate(BaseModel):
